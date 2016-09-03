69cfead8f0e7c99ed5236ddb0eda919e05ec508b
Taranveer Bains <taranveer786@gmail.com>
2016-09-03 10:01:20 -0700

    Going to use css and load the content for the top section to avoid writing javascript

--- js/app.js
+++ js/app.js
@@ -1,23 +1,35 @@
-var container = document.querySelector('.testy');
-var bro = document.querySelector('.bro');
+// var container = document.querySelector('.testy');
+// var bro = document.querySelector('.bro');
+//
+// container.addEventListener('click', function(e) {
+//     if (e.target !== e.currentTarget) {
+//         e.preventDefault();
+//         var aLink = e.target.getAttribute('href');
+//         history.pushState(null, null, aLink);
+//
+//         $.ajax({
+//             url: aLink,
+//             success: function(data) {
+//                 console.log(data);
+//                 bro.innerHTML = data;
+//             },
+//             error: function(jqXHR, textStatus, errorThrown) {
+//                 console.log(textStatus, errorThrown);
+//                 alert('Failed');
+//             }
+//         });
+//     }
+//     e.stopPropagation();
+// }, false);
+window.addEventListener('hashchange', function() {
+    console.log('this view\'s id is ', location.hash.substr(1));
+    var current_view = document.querySelector("a[href='" + location.hash + "']");
+    var nav_ul = document.querySelector('.nav-ul');
 
-container.addEventListener('click', function(e) {
-    if (e.target !== e.currentTarget) {
-        e.preventDefault();
-        var aLink = e.target.getAttribute('href');
-        history.pushState(null, null, aLink);
-
-        $.ajax({
-            url: aLink,
-            success: function(data) {
-                console.log(data);
-                bro.innerHTML = data;
-            },
-            error: function(jqXHR, textStatus, errorThrown) {
-                console.log(textStatus, errorThrown);
-                alert("Failed");
-            }
-        });
+    for (var i = 0; i < nav_ul.children.length; i++) {
+      if(nav_ul.children[i].firstChild.classList.contains('active')){
+        nav_ul.children[i].firstChild.classList.remove('active');
+      }
     }
-    e.stopPropagation();
-}, false);
+    current_view.classList.add("active");
+});
