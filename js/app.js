f6b23b29db17f6244c31da03dce2a22eb4622ebb
Taranveer Bains <taranveer786@gmail.com>
2016-09-09 14:43:06 -0700

    Making ajax calls as opposed to the old way. Need to fix up the navbar and make and set default text

--- js/app.js
+++ js/app.js
@@ -1,45 +1,31 @@
 $(document).ready(function() {
-    addActiveClassToNav();
-    $('.nav-toggle').click(function(e) {
-        e.preventDefault();
-        toggleNavbar();
-    });
+    var navbar = document.querySelector('.nav-ul');
+    var content = document.querySelector('.main');
 
-    $('.nav-ul').on('click', 'li', function(e) {
-      toggleNavbar();
+    navbar.addEventListener('click', function(e) {
+
+        if (e.target !== e.currentTarget) {
+            e.preventDefault();
+            var aLink = e.target.getAttribute('href');
+            var data = aLink.replace(".html", "");
+            data = data.replace("/", "");
+
+            history.pushState(data, null, aLink);
+            requestContent(aLink);
+        }
+        e.stopPropagation();
+    }, false);
+
+    window.addEventListener('popstate', function(e) {
+        var page = e.state;
+        if (page !== null) {
+            requestContent(page + ".html");
+        }
     });
 });
 
-window.addEventListener('hashchange', function() {
-    addActiveClassToNav();
-});
-
-var removeNavActive = function() {
-    var nav_ul = document.querySelector('.nav-ul');
-    for (var i = 0; i < nav_ul.children.length; i++) {
-        if (nav_ul.children[i].firstChild.classList.contains('active')) {
-            nav_ul.children[i].firstChild.classList.remove('active');
-        }
-        if (nav_ul.children[i].classList.contains('active')) {
-            nav_ul.children[i].classList.remove('active');
-        }
-    }
-};
-
-var addActiveClassToNav = function() {
-    if (location.hash !== "") {
-        var current_view = document.querySelector("a[href='" + location.hash + "']").parentElement;
-        removeNavActive();
-        current_view.classList.add("active");
-    }
-};
-
-var toggleNavbar = function() {
-    if ($('.nav').hasClass('open')) {
-        $('.nav').removeClass('open');
-        $('.mobile-nav-toggle').removeClass('active-nav-button');
-    } else {
-        $('.nav').addClass('open');
-        $('.mobile-nav-toggle').addClass('active-nav-button');
-    }
+var requestContent = function(file) {
+    $.get(file, function(data) {
+        $('.main').html(data);
+    });
 };
