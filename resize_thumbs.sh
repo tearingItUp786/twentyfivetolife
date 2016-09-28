4a69a076840674436b39939ffab4b1cffe33f15a
Taranveer Bains <taranveer786@gmail.com>
2016-09-27 21:46:03 -0700

    Got a shell script to resize the images and make them black and white, now need to think about the image directory structure and come up with a grunt command

---
+++ resize_thumbs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ./images/sponsor_logos
+convert *.jpg -resize 200x200 -fuzz 10% -transparent white -threshold 0 -negate -set filename:base "%[base]" "%[filename:base].png"
+mogrify *.png -path ../sponsor_thumbnails *.png
