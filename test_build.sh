d4237abeda9085d5b1a07c8691f95f72fee85ff4
Taranveer Bains <taranveer786@gmail.com>
2016-10-04 01:47:53 -0700

    Deleted wallop.js file. Updated config.yml to exclude my shell scripts from _site. Updated index.html default hex paragraph and header content. Created test_build.sh to test script that creates directory in _site directory.

---
+++ test_build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd ./_site/images
+mkdir sponsor_thumbnails
+cd ./sponsor_logos
+convert *.jpg -resize 200x200 -fuzz 10% -transparent white -threshold 0 -negate -set filename:base "%[base]" "%[filename:base].png"
+mogrify *.png -path ../sponsor_thumbnails *.png
